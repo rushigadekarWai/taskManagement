940eb7d65c1e78e78684dc24012d7e20c94ac7e7
rushigadekarWai <rushikesh.gadekar@waiin.com>
2025-04-04 18:27:39 +0530

    Initial commit

---
+++ api/db.js
@@ -0,0 +1,13 @@
+// api/db.js
+
+const jsonServer = require('json-server');
+const path = require('path');
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, '../db.json'));
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+module.exports = server;
